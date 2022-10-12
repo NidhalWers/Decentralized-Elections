34d1b030870fa1c973609f80dfd090c2e934663c
mohamedzenatiofficiel <91180485+mohamedzenatiofficiel@users.noreply.github.com>
2022-10-12 11:22:35 +0200

    Front V1

--- Application/SmartVote/AdminSpace/urls.py
+++ Application/SmartVote/AdminSpace/urls.py
@@ -3,5 +3,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.adminspace, name='admin'),
+    path('alpha', views.alpha, name='admin'),
+    path('administrateur', views.administrateur, name='administrateur'),
+    path('beta', views.beta, name='beta'),
+    path('gamma', views.gamma, name='gamma'),
+    path('lamda', views.lamnda, name='lamda'),
+    path('epsilon', views.epsilon, name='epsilon'),
+    path('zelda', views.zelda, name='zelda'),
+    
 ]
