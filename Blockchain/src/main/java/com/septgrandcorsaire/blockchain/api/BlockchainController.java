d5694022547bd22b2c687179e6e4e693fd27a057
NidhalWers <32906777+NidhalWers@users.noreply.github.com>
2022-11-16 17:10:55 +0100

    B/bug#38 (#34)
    * [B/Bug#38] Autoriser CORS

--- Blockchain/src/main/java/com/septgrandcorsaire/blockchain/api/BlockchainController.java
+++ Blockchain/src/main/java/com/septgrandcorsaire/blockchain/api/BlockchainController.java
@@ -1,5 +1,6 @@
 package com.septgrandcorsaire.blockchain.api;
 
+import com.septgrandcorsaire.blockchain.Application;
 import com.septgrandcorsaire.blockchain.api.error.exception.ElectionAlreadyFinishedException;
 import com.septgrandcorsaire.blockchain.api.payload.ElectionPayload;
 import com.septgrandcorsaire.blockchain.api.payload.VotePayload;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
  * @author Nidhal TEYEB
  * @since 0.0.1-SNAPSHOT
  */
+@CrossOrigin
 @RestController
 public class BlockchainController {
 
@@ -31,12 +33,14 @@ public class BlockchainController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public BlockChainResource createElection(@RequestBody ElectionPayload payload) {
+        Application.LOGGER.info("GET /smart-vote/api/v1/create-election");
         MessageBlockchainCreated result = electionApplicationService.createBlockchainForElection(payload.toQuery());
         return BlockChainResource.of(result.blockChain, result.apiKey);
     }
 
     @GetMapping(value = "/smart-vote/api/v1/get-election/{election_name}")
     public ElectionResource getElection(@PathVariable("election_name") String input) {
+        Application.LOGGER.info("GET /smart-vote/api/v1/get-election/" + input);
         BlockChain result;
         try {
             result = electionApplicationService.getElectionData(input);
@@ -48,6 +52,7 @@ public class BlockchainController {
 
     @GetMapping(value = "/smart-vote/api/v1/get-sandbox/")
     public ElectionResource getSandboxElection() {
+        Application.LOGGER.info("GET /smart-vote/api/v1/get-sandbox/");
         return getElection("sandbox");
     }
 
@@ -55,6 +60,7 @@ public class BlockchainController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public BlockResource voteInElection(@RequestBody VotePayload payload) {
+        Application.LOGGER.info("POST /smart-vote/api/v1/vote");
         Block result = electionApplicationService.voteInElection(payload.toQuery());
         return BlockResource.of(result);
     }
