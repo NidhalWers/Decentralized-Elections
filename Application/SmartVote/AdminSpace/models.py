22cda35593ed544b2ce67b2c64ccbc0cb5893675
William C <chenut.william@gmail.com>
2022-11-23 16:13:37 +0100

    Update Election

--- Application/SmartVote/AdminSpace/models.py
+++ Application/SmartVote/AdminSpace/models.py
@@ -13,3 +13,5 @@ class Election(models.Model):
     ElectionCandidates = models.CharField(max_length=10000, blank=True , null=True)
     ElectionStatus = models.CharField(max_length=200, blank=True , null=True)
     ElectionApiKey = models.CharField(max_length=500, blank=True , null=True)
+    ElectionStartDate = models.DateTimeField(auto_now_add=False, blank=True, null=True)
+    ElectionEndDate = models.DateTimeField(auto_now_add=False, blank=True, null=True)
