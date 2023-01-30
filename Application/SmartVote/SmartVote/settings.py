5d8ff722b88ab0544e82fa6b0f926724f8899487
William C <chenut.william@gmail.com>
2023-01-30 11:26:56 +0100

    Update settings.py

--- Application/SmartVote/SmartVote/settings.py
+++ Application/SmartVote/SmartVote/settings.py
@@ -144,8 +144,8 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://localhost:8080",
+    "http://127.0.0.1:8080"
 ]
   
 # Path where media is stored  
