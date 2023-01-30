b5087860ba8e31bda7a4c1b818689018bfed114d
William C <chenut.william@gmail.com>
2023-01-30 12:08:29 +0100

    Print candidate in parametre

--- Application/SmartVote/SmartVote/settings.py
+++ Application/SmartVote/SmartVote/settings.py
@@ -143,10 +143,6 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
     'X-CSRFTOKEN',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8080"
-]
   
 # Path where media is stored  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  
